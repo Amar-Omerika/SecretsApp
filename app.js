ab538ab5db8a3ea6a224c74a45db45a220e2afce
Amar Omerika <amar.omerika@edu.fit.ba>
2021-03-28 20:10:31 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	console.log("Radi");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
