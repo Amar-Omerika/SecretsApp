36d60c4c2958bcb62576b4b9decc8e3a3c4e9369
Amar Omerika <amar.omerika@edu.fit.ba>
2021-03-31 23:03:04 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -22,8 +22,11 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
 });
-const secret = "Thisisoutlittlesecret ";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt, {
+	secret: process.env.SECRET,
+	encryptedFields: ["password"],
+});
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
