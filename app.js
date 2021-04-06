f76208019ce719062b046e3bae953046d8255412
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-06 12:32:00 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -14,9 +14,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+app.use(
+	session({
+		secret: "Our little secret.",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
 	useNewUrlParser: true,
