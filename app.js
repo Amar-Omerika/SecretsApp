e43d67e4402d326814f793d1d80697c8e105dded
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-06 12:24:26 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const ejs = require("ejs");
 const path = require("path");
 const mongoose = require("mongoose");
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
 const app = express();
 const port = 3000;
 
@@ -38,38 +35,8 @@ app.get("/register", (req, res) => {
 	res.render("register");
 });
 
-app.post("/register", (req, res) => {
-	bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-		const newUser = new User({
-			email: req.body.username,
-			password: hash,
-		});
-		newUser.save(function (err) {
-			if (err) {
-				console.log(err);
-			} else {
-				res.render("secrets");
-			}
-		});
-	});
-});
-app.post("/login", (req, res) => {
-	const username = req.body.username;
-	const password = req.body.password;
-	User.findOne({ email: username }, function (err, foundUser) {
-		if (err) {
-			console.log(err);
-		} else {
-			if (foundUser) {
-				bcrypt.compare(password, foundUser.password, function (err, result) {
-					if (result === true) {
-						res.render("secrets");
-					}
-				});
-			}
-		}
-	});
-});
+app.post("/register", (req, res) => {});
+app.post("/login", (req, res) => {});
 app.listen(port, () => {
 	console.log(`Server is listening at http://localhost:${port}`);
 });
