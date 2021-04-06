5fe975ba692015b1cdd90457f9ad533f5ecb2572
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-06 13:35:28 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+//this section here of code needs to be placed right here
 app.use(
 	session({
 		secret: "Our little secret.",
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.set("useCreateIndex", true);
-
+//------------------------------------------
 mongoose.connect("mongodb://localhost:27017/userDB", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -54,8 +54,30 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
 	res.render("register");
 });
+app.get("/secrets", (req, res) => {
+	if (req.isAuthenticated()) {
+		res.render("secrets");
+	} else {
+		res.redirect("/login");
+	}
+});
 
-app.post("/register", (req, res) => {});
+app.post("/register", (req, res) => {
+	User.register(
+		{ username: req.body.username },
+		req.body.password,
+		function (err, user) {
+			if (err) {
+				console.log(err);
+				res.redirect("/register");
+			} else {
+				passport.authenticate("local")(req, res, function () {
+					res.redirect("/secrets");
+				});
+			}
+		}
+	);
+});
 app.post("/login", (req, res) => {});
 app.listen(port, () => {
 	console.log(`Server is listening at http://localhost:${port}`);
