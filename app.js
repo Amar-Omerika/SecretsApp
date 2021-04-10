03ef4f3f3e4d4cf23fa5ce7ac068523c047687cc
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-10 13:01:25 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -44,8 +44,15 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
 
 passport.use(
 	new GoogleStrategy(
