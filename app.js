d8bbd1bc1f03ee94aa633ec5ac99577c71a22dbc
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-10 13:04:06 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get(
 	passport.authenticate("google", { failureRedirect: "/login" }),
 	function (req, res) {
 		// Successful authentication, redirect home.
-		res.redirect("/");
+		res.redirect("/secrets");
 	}
 );
 app.get("/login", (req, res) => {
