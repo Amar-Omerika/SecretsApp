6b0965cfed81e0dad4ff6f6e7adcb61bdc7e6d51
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-16 16:50:10 +0200

    SecretsApp

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(
 		},
 	})
 );
+
 /*app.use(
 	session({
 		secret: process.env.SECRET || "Ourlittlesecret",
@@ -208,6 +209,10 @@ app.post("/login", function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
-	console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 3000;
+}
+app.listen(port, function () {
+	console.log("Server has started on port 3000 ");
 });
